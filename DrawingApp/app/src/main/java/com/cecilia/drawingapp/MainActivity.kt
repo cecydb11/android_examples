7afae01b24f8d194da8512db32df4210b0c84823
Cecilia <jceciliadavilaarandas@gmail.com>
2020-12-14 23:20:34 -0600

    Implement brush size selector

--- DrawingApp/app/src/main/java/com/cecilia/drawingapp/MainActivity.kt
+++ DrawingApp/app/src/main/java/com/cecilia/drawingapp/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.cecilia.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawing_view.setSizeForBrush(22.toFloat())
+        ib_brush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+        brushDialog.show()
+
+        val smallButton = brushDialog.ib_small_brush
+        smallButton.setOnClickListener {
+             drawing_view.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        val mediumButton = brushDialog.ib_medium_brush
+        mediumButton.setOnClickListener {
+            drawing_view.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val largeButton = brushDialog.ib_large_brush
+        largeButton.setOnClickListener {
+            drawing_view.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
     }
 }
