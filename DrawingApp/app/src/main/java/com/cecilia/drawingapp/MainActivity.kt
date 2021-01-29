8337a86518b98cffa0e1df6f41423ee0c373e074
Cecilia <jceciliadavilaarandas@gmail.com>
2021-01-28 23:04:13 -0600

    Add function to change background, and to undo lines.

--- DrawingApp/app/src/main/java/com/cecilia/drawingapp/MainActivity.kt
+++ DrawingApp/app/src/main/java/com/cecilia/drawingapp/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.cecilia.drawingapp
 
+import android.Manifest
+import android.app.Activity
 import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +31,36 @@ class MainActivity : AppCompatActivity() {
         mImageButtonCurrentPaint!!.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.pallet_selected))
         ib_brush.setOnClickListener {
             showBrushSizeChooserDialog()
+        }
+        ib_gallery.setOnClickListener {
+            if(isReadStorageAllowed()){
+                val pickPhotoIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(pickPhotoIntent, GALLERY)
+            }else{
+                requestStoragePermission()
+            }
+        }
+        ib_undo.setOnClickListener {
+            drawing_view.onClickUndo()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == GALLERY){
+                try{
+                    if(data!!.data != null){
+                        iv_background.visibility = View.VISIBLE
+                        iv_background.setImageURI(data.data)
+                    }else{
+                        Toast.makeText(this,"Error in parsing the image.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }catch(e: Exception){
+                    e.printStackTrace()
+                }
+            }
         }
     }
 
@@ -48,7 +86,6 @@ class MainActivity : AppCompatActivity() {
             drawing_view.setSizeForBrush(30.toFloat())
             brushDialog.dismiss()
         }
-
     }
 
     fun paintClicked(view: View){
@@ -60,6 +97,43 @@ class MainActivity : AppCompatActivity() {
             mImageButtonCurrentPaint!!.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.pallet_normal))
             mImageButtonCurrentPaint = view
         }
+    }
 
+    private fun requestStoragePermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE).toString())){
+            Toast.makeText(this,"You need permission to add a background image.",
+                Toast.LENGTH_SHORT).show()
+        }
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"Permission to read storage was granted.",
+                    Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this,"You need permission to read storage..",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isReadStorageAllowed(): Boolean{
+        val result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object{
+        private const val STORAGE_PERMISSION_CODE = 1
+        private const val GALLERY = 2
     }
 }
